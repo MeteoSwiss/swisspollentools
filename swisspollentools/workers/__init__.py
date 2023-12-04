fc4716e503530bb43807ddba1b4fc62e8e77b809
AxelGiottonini <giottonini.axel@gmail.com>
2023-12-04 15:25:20 +0100

    Fix packaging import

--- swisspollentools/workers/__init__.py
+++ swisspollentools/workers/__init__.py
@@ -1,4 +1,5 @@
 from swisspollentools.workers.extraction import *
 from swisspollentools.workers.inference import *
+from swisspollentools.workers.merge import *
 from swisspollentools.workers.tocsv import *
 from swisspollentools.workers.tohdf5 import *
---
+++ swisspollentools/workers/merge/__init__.py
@@ -0,0 +1,3 @@
+from swisspollentools.workers.merge.config import MergeWorkerConfig
+from swisspollentools.workers.merge.messages import MergeRequest, MergeResponse, MeReq, MeRes
+from swisspollentools.workers.merge.worker import MergeWorker, Merge
