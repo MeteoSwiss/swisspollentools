343403edb522f879acd3a8b0652bc38a940315c5
AxelGiottonini <giottonini.axel@gmail.com>
2024-01-17 10:33:08 +0100

    New Feature: confusion matrix function in model_qc

--- swisspollentools/new_features/model_qc/model_qc.py
+++ swisspollentools/new_features/model_qc/model_qc.py
@@ -1,6 +1,14 @@
 """
 This module provides the tools to work on the SPT Inference pipeline outputs.
 
+Functions:
+----------
+- get_poleno_data
+- get_hirst_data
+- stats_fn
+- confusion_matrix_fn
+- model_qc
+
 Example:
 --------
 ```
@@ -80,12 +88,10 @@ for _, row in tqdm.tqdm(poleno_station.iterrows()):
         poleno_data.to_csv(out_file_name)
 ```
 """
-
-import os
 import glob
 
 from datetime import date, datetime, timedelta
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Tuple, Callable
 
 import numpy as np
 import scipy as sp
@@ -97,7 +103,7 @@ def get_poleno_data(
     supervisor_params: dict,
     min_date: Optional[date]=None,
     max_date: Optional[date]=None,
-    atol: float=1e-1
+    atol: float=1e-1,
 ) -> pd.DataFrame:
     """
     The function is responsible to read files created with the SPT inference
@@ -106,17 +112,17 @@ def get_poleno_data(
     Parameters:
     -----------
     - poleno_paths: A list of .csv files produced with the SPT inference pipeline
-        or a list of directories containing the files
+    or a list of directories containing the files
     - model_class_dict: Translation dictionnary from integer class to named 
-        class, i.e. `{0: "alnus", 1: "betula", 2: "corylus"}`
+    class, i.e. `{0: "alnus", 1: "betula", 2: "corylus"}`
     - supervisor_params: Dictionary with the parameters for the supervisor as
-        `{key: (threshold_soft, threshold_hard, supervisor_numb)}`
+    `{key: (threshold_soft, threshold_hard, supervisor_numb)}`
     - min_date: The starting date for the time series, default value is the min
-        date in the dataset
+    date in the dataset
     - max_date: The starting date for the time series, default value is the max
-        date in the dataset
+    date in the dataset
     - atol: Tolerance for the verification that prediction/probs field contains
-        probabilities
+    probabilities
 
     Raise:
     ------
@@ -190,7 +196,8 @@ def get_poleno_data(
     data = data[["metadata/date", "prediction/class", "prediction/certainity"]]
 
     # Name the predicted class
-    data["prediction/class"] = data["prediction/class"].apply(lambda x: model_class_dict[x])
+    data["prediction/class"] = data["prediction/class"] \
+        .apply(lambda x: model_class_dict[x])
 
     # Soft & hard filter for the supervisor
     soft_filter = data \
@@ -246,8 +253,8 @@ def get_poleno_data(
                 for i in range(int((max_date - min_date) / delta))
         ]
     )
-    soft_data = real_index.join(soft_data).sort_index()
-    hard_data = real_index.join(hard_data).sort_index()
+    soft_data = real_index.join(soft_data).sort_index().sort_index(axis=1)
+    hard_data = real_index.join(hard_data).sort_index().sort_index(axis=1)
 
     # Compute the supervisor mask
     supervisor = hard_data \
@@ -283,11 +290,11 @@ def get_hirst_data(
     - hirst_path: Path to the csv file containing the hirst data
     - station_id: Identifier of the poleno/station of interest
     - hirst_class_dict: Translation dictionnary from integer class to named 
-        class, i.e. `{"4837": "alnus", "4839": "betula", "4844": "corylus"}`
+    class, i.e. `{"4837": "alnus", "4839": "betula", "4844": "corylus"}`
     - min_date: The starting date for the time series, default value is the min
-        date in the dataset
+    date in the dataset
     - max_date: The starting date for the time series, default value is the max
-        date in the dataset
+    date in the dataset
     - nan_value: list of values that should be replaced by a `np.nan`
 
     Return:
@@ -298,7 +305,7 @@ def get_hirst_data(
     --------
     ```
     hirst_path = "../../../../swisspollen-reanalysis/data/donnes_manuelles.csv"
-    poleno_id = 965
+    station_id = 965
     hirst_class_dict = {"4837": "alnus", "4839": "betula", "4844": "corylus"}
 
     hirst_data = get_hirst_data(
@@ -424,6 +431,12 @@ def stats_fn(
         key_poleno = key + "Poleno"
         key_hirst = key + "Hirst"
 
+        # Ignore the statistics when the joined table is empty
+        if len(joined) == 0:
+            statistics[key] = (-1,)*6
+            continue
+
+        # Ignore the statistics when the poleno column is constant
         if (joined[key_poleno] == joined[key_poleno].iloc[0]).all():
             statistics[key] = (-1,)*6
             continue
@@ -471,8 +484,102 @@ def stats_fn(
         "Kendall", "Spearman", "Tailedness",
         "SamplingMean", "SamplingCiInf", "SamplingCiSup"
     ]
-
+    statistics = statistics.sort_index()
     return statistics
+
+def confusion_matrix_fn(
+    poleno_data: pd.DataFrame,
+    hirst_data: pd.DataFrame,
+    threshold: float=0.2
+):
+    """
+    This function compute a pseudo-confusion matrix to have an approximation of
+    how much Poleno time series aggregate different Hirst time series.
+
+    Parameters:
+    -----------
+    - poleno_data: a dataframe computed with the get_poleno_data function
+    - hirst_data: a dataframe computed with the get_hirst_data function
+    - threshold: a float for filtering the valid Hirst regressor after first
+    leas-squares iteration
+
+    Returns:
+    --------
+    - pd.DataFrame
+
+    Example:
+    --------
+    ```
+    poleno_paths = [
+        "../../../../swisspollen-reanalysis/results/2021/3/poleno-4/0/"
+    ]
+    hirst_path = "../../../../swisspollen-reanalysis/data/donnes_manuelles.csv"
+    poleno_id = 965
+    model_class_dict = {0: "alnus", 1: "betula", 2: "corylus"}
+    hirst_class_dict = {"4837": "alnus", "4839": "betula", "4844": "corylus"}
+    supervisor_params = {
+        "alnus": (0.85, 0.998, 100),
+        "betula": (0.95, 0.999, 50),
+        "corylus": (0.97, 0.9999, 100)
+    }
+
+    poleno_data = get_poleno_data(
+        poleno_paths,
+        model_class_dict,
+        supervisor_params
+    )
+    hirst_data = get_hirst_data(
+        hirst_path,
+        poleno_id,
+        hirst_class_dict
+    )
+
+    confusion_matrix = confusion_matrix_fn(poleno_data, hirst_data)
+    ```
+
+    Note:
+    -----
+    - There is no formal proof regarding the quality of this statistic, it is
+    principally designed to avoid the visual inspection of all the Poleno time
+    series with various combination of the Hirst time series.
+    """
+    # Assert the poleno dataframe and hirst dataframe have the same columns
+    keys = set(poleno_data.columns).intersection(set(hirst_data.columns))
+    assert len(keys) == len(poleno_data.columns)
+
+    # Standardize the time series between 0 and 1
+    poleno_data = poleno_data \
+        .apply(lambda col: (col - col.min())/(col.max() - col.min()), axis=0)
+    hirst_data = hirst_data \
+        .replace(np.nan, 0) \
+        .apply(lambda col: (col - col.min())/(col.max() - col.min()), axis=0)
+    
+    # Compute the confusion matrix
+    confusion_matrix = []
+    for key in keys:
+        # First iteration of the lstsq
+        betas = np.linalg.lstsq(hirst_data.values, poleno_data[key].values)[0]
+
+        # Filter the Hirst regressor based on first lstsq iteration (i.e.
+        # removing negative betas as they are not coherent)
+        regressor_keys = hirst_data.columns[betas >= threshold]
+        regressor_values = hirst_data.values[:, betas >= threshold]
+
+        # Second iteration of the lstsq with the filtered regressors
+        betas, residuals, _, _ = \
+            np.linalg.lstsq(regressor_values, poleno_data[key].values)
+        
+        # Normalize the betas so that they sum to 1
+        betas = (betas / sum(betas)).round(3)
+        confusion_matrix.append(
+            {**dict(zip(regressor_keys, betas)), "residuals": residuals[0]}
+        )
+
+    confusion_matrix = pd.DataFrame(confusion_matrix, index=list(keys)) \
+        .replace(np.nan, 0) \
+        .sort_index() \
+        .sort_index(axis=1)
+    return confusion_matrix
 
 def model_qc(
     poleno_paths: List[str],
@@ -485,8 +592,13 @@ def model_qc(
     max_date: Optional[date]=None,
     atol: float=1e-1,
     nan_value: Any=32767,
-    flow: float=40 * 0.001 / 60
-) -> pd.DataFrame:
+    qc_fns: List[
+        Tuple[Callable[[pd.DataFrame, pd.DataFrame, Any], pd.DataFrame], Any]
+    ]=[
+        (stats_fn, 40 * 0.001 / 60),
+        (confusion_matrix_fn, 0.2)
+    ]
+) -> List[pd.DataFrame]:
     """
     This function compute the various statistics for the model quality check 
     given a list of files produced with the SPT Inference pipeline and a Hirst
@@ -494,32 +606,37 @@ def model_qc(
     with integers indicating the poleno ids that will be used to filter the 
     data frame. The file also has the `JAHR`, `MO`, `TG` columns to specify the
     date. Such will can be obtained using Climap with outputs a tabular file 
-    than can then be processed, i.e. using awk, to get a csv file.
+    than can then be processed, i.e. using awk, to get a csv file. A list of qc
+    function can be passed as arguments to compute different statistics. By
+    default, the module defined statistics function and confusion matrix
+    functions are used.
 
     Parameters:
     -----------
     - poleno_paths: A list of .csv files produced with the SPT inference pipeline
-        or a list of directories containing the files
+    or a list of directories containing the files
     - hirst_path: Path to the csv file containing the hirst data
     - station_id: Identifier of the poleno/station of interest
     - model_class_dict: Translation dictionnary from integer class to named 
-        class, i.e. `{0: "alnus", 1: "betula", 2: "corylus"}`
+    class, i.e. `{0: "alnus", 1: "betula", 2: "corylus"}`
     - hirst_class_dict: Translation dictionnary from integer class to named 
-        class, i.e. `{"4837": "alnus", "4839": "betula", "4844": "corylus"}
+    class, i.e. `{"4837": "alnus", "4839": "betula", "4844": "corylus"}
     - supervisor_params: Dictionary with the parameters for the supervisor as
-        `{key: (threshold_soft, threshold_hard, supervisor_numb)}`
+    `{key: (threshold_soft, threshold_hard, supervisor_numb)}`
     - min_date: The starting date for the time series, default value is the min
-        date in the dataset
+    date in the dataset
     - max_date: The starting date for the time series, default value is the max
-        date in the dataset
+    date in the dataset
     - atol: Tolerance for the verification that prediction/probs field contains
-        probabilities`
+    probabilities`
     - nan_value: list of values that should be replaced by a `np.nan`
-    - flow: poleno air flow in m^3/s
+    - qc_fns: list of quality control functions and their optional arguments as
+    tuples. The quality control functions take as first argument the Poleno
+    dataframe and as second argument the Hirst dataframe.
     
     Return:
     -------
-    - pd.DataFrame
+    - list of pd.DataFrame
 
     Example:
     --------
@@ -534,7 +651,7 @@ def model_qc(
     statistics = model_qc(
         poleno_paths,
         hirst_path,
-        poleno_id,
+        station_id,
         model_class_dict,
         hirst_class_dict,
         supervisor_params
@@ -557,9 +674,5 @@ def model_qc(
         max_date=max_date,
         nan_value=nan_value
     )
-    stats = stats_fn(
-        poleno_data=poleno_data,
-        hirst_data=hirst_data,
-        flow=flow
-    )
-    return stats
+
+    return [(qc_fn(poleno_data, hirst_data, *args)) for qc_fn, *args in qc_fns]
